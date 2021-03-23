7c42b811d63a7089a4b9eb7520acd276973cd700
parsa-script <mihandoost.p@gmail.com>
2021-03-23 16:04:41 +0430

    add consumer with seed

--- src/main/java/ir/codefather/ConsumerSeekDemo.java
+++ src/main/java/ir/codefather/ConsumerSeekDemo.java
@@ -8,11 +8,12 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ConsumerDemo {
+public class ConsumerSeekDemo {
 
     static Logger logger = LoggerFactory.getLogger(ConsumerDemo.class);
 
@@ -21,22 +22,22 @@ public class ConsumerDemo {
         properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "java_application");
-        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        // properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "java_application");
+        // properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
         KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(properties);
-        kafkaConsumer.subscribe(Arrays.asList("first_topic"));
+        TopicPartition topicPartition = new TopicPartition("first_topic", 0);
+        kafkaConsumer.assign(Arrays.asList(topicPartition));
+        kafkaConsumer.seek(topicPartition, 15);
+        
+        ConsumerRecords<String, String> records = kafkaConsumer.poll(Duration.ofMillis(1000));
 
-        /**
-         * this is bad cause we cannot close consumer and it might be availible when even with ctrl+c
-         */
-        while (true) {
-            ConsumerRecords<String, String> records = kafkaConsumer.poll(Duration.ofMillis(1000));
-
-            for (ConsumerRecord<String, String> record : records) {
-                logger.info("record info: partition:" + record.partition() + " - offset:" + record.offset());
-                logger.info("record info: key:" + record.key() + " - value:" + record.value());
-            }
+        for (ConsumerRecord<String, String> record : records) {
+            logger.info("record info: partition:" + record.partition() + " - offset:" + record.offset());
+            logger.info("record info: key:" + record.key() + " - value:" + record.value());
         }
+
+        kafkaConsumer.close();
     }
+
 }
